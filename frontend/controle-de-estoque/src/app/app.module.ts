90141237880634ad17426f565c5d3a7bcb5ce530
rodrigu3s <joaorodrigues9631@gmail.com>
2024-08-01 22:28:16 -0300

    mudando para versao 14 e adicionando o  angular material

--- frontend/controle-de-estoque/src/app/app.module.ts
+++ frontend/controle-de-estoque/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatSidenavModule
   ],
   providers: [],
   bootstrap: [AppComponent]
